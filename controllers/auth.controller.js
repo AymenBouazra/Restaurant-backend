25c81ea87c2f671c21d1f935b84592f4f62bd7ea
Aymen <aymenbouazra994@gmail.com>
2023-01-06 16:44:54 +0100

    bcrypt

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const Auth = require('../models/auth')
-
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 exports.register = async (req,res) => {
     try {
@@ -7,6 +8,8 @@ exports.register = async (req,res) => {
         if (found) {
             res.status(400).json({message:'User already exists'})
         }
+        const salt = bcrypt.genSalt(10);
+        bcrypt.hashSync(req.body.password,salt).then((hash)=> { req.body.password = hash})
         await Auth.create(req.body)
         res.json({message:'User registered successfully'})
     } catch (error) {
@@ -16,7 +19,10 @@ exports.register = async (req,res) => {
 
 exports.login = async (req,res) => {
     try {
-        
+        const found = await Auth.findOne({email:req.body.email})
+        if(found){
+            
+        }
     } catch (error) {
         res.status(500).json({message:error.message || 'Error server'})
     }
