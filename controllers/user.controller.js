76f492ada34d567dbb403736cb903090ee61dd5e
Aymen <aymenbouazra994@gmail.com>
2023-01-18 23:37:41 +0100

    added upload photo in user and delete file when update and delete

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,7 @@
 const User = require('../models/auth');
-const bcrypt  = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const path = require('path');
+const fs = require('fs')
 exports.getAllUsers = async (req, res) => {
     try {
         const Users = await User.find();
@@ -24,6 +26,7 @@ exports.createUser = async (req, res) => {
         if (userExist) {
             res.status(400).json({ message: 'User already exists with this e-mail address!' })
         } else {
+            req.body.photo = 'http://localhost:4000/uploads/' + req.file.filename
             const salt = bcrypt.genSaltSync(10);
             const hash = bcrypt.hashSync(req.body.password, salt)
             req.body.password = hash
@@ -38,23 +41,39 @@ exports.createUser = async (req, res) => {
 exports.updateUserById = async (req, res) => {
     try {
         const user = await User.findById(req.params.id);
-        if (req.body.password == '') {
+        if (!req.body.password) {
             req.body.password = user.password
-        }else{
+        } else {
             const salt = bcrypt.genSaltSync(10);
             const hash = bcrypt.hashSync(req.body.password, salt)
             req.body.password = hash
         }
+        if (req.file) {
+            req.body.photo = 'http://localhost:4000/uploads/' + req.file.filename
+            const fileName = path.basename(user.photo);
+            const filePath = path.resolve('./uploads', fileName);
+            if (fs.existsSync(filePath)) {
+                fs.unlinkSync(filePath);
+            }
+        }
         await User.findByIdAndUpdate(req.params.id, req.body);
         res.json({ message: 'User updated successfully' })
     } catch (error) {
+        console.log(error);
         res.status(500).json({ message: error.message || 'Server error!' })
     }
 }
 
 exports.deleteUserById = async (req, res) => {
     try {
-        const Users = await User.findByIdAndDelete(req.params.id);
+        const user = await User.findById(req.params.id);
+
+        const fileName = path.basename(user.photo);
+        const filePath = path.resolve('./uploads', fileName);
+        if (fs.existsSync(filePath)) {
+            fs.unlinkSync(filePath);
+        }
+        await User.findByIdAndDelete(req.params.id);
         res.json({ message: 'User deleted successfully' })
     } catch (error) {
         res.status(500).json({ message: error.message || 'Server error!' })
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const passport      = require('passport');
 const session       = require('express-session');
 const app           = express()
 const port          = 4000
+const path          = require('path');
+
 require('dotenv').config()
 require('./passport/bearer')
 require('./common/init_scripts/int_script')
@@ -17,6 +19,7 @@ app.use(express.urlencoded({ limit: '100mb', extended: false }))
 app.use(express.json({limit: '100mb'}))
 app.use(bodyParser.json({ limit: 50 * 1024 * 1024 }))
 app.use(bodyParser.urlencoded({ limit: 50 * 1024 * 1024, extended: true, parameterLimit: 50000 }))
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use(session({ resave: true, secret: process.env.JWT_SECRET, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
