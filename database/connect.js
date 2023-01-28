4fee8cba1f4774e30bceea7651f92ec98789eb20
Aymen <aymenbouazra994@gmail.com>
2023-01-28 21:37:35 +0100

    first commit

--- database/connect.js
+++ database/connect.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-const local = 'mongodb://127.0.0.1:27017/e-commerce-app'
-const devolpment = 'mongodb+srv://cv-generator:cv123456@mydatabase.8vzuo.mongodb.net/e-commerce-app'
+const local = 'mongodb://127.0.0.1:27017/restaurant'
+const devolpment = 'mongodb+srv://cv-generator:cv123456@mydatabase.8vzuo.mongodb.net/restaurant'
 mongoose.set('strictQuery', false);
-mongoose.connect(devolpment).then(success=>{
+mongoose.connect(devolpment).then(success => {
     console.log('Successfuly connected to database');
-}).catch(error=>{
+}).catch(error => {
     console.log('error connecting to database');
 })
